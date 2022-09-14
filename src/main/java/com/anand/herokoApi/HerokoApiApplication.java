074ac907d2e097f539c774ecbbf10259df356a08
Anand kumar <anandkmr782@gmail.com>
2022-09-14 14:27:48 +0530

    anand

---
+++ src/main/java/com/anand/herokoApi/HerokoApiApplication.java
@@ -0,0 +1,13 @@
+package com.anand.herokoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokoApiApplication.class, args);
+	}
+
+}
